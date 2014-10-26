2df831f23344876ef2ed5ea041d8f5df287a86ad
Thomas Chen <foxnewsnetwork@gmail.com>
2014-10-25 22:43:15 -0700

    put in a thing to force translations

--- app/controllers/apiv1/i18n_translations/index_controller.rb
+++ app/controllers/apiv1/i18n_translations/index_controller.rb
@@ -1,5 +1,11 @@
 class Apiv1::I18nTranslations::IndexController < ApplicationController
   def index
+    _force_all_lazy_translations_to_load
     render json: { translations: I18n.backend.send(:translations), locale: I18n.locale }
   end
+  private
+  def _force_all_lazy_translations_to_load
+    I18n.t :dog
+  end
+
 end
