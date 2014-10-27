9259f32ff8a109768b8295134317961113a91d6a
Thomas Chen <foxnewsnetwork@gmail.com>
2014-10-26 21:31:00 -0700

    switched up the conf files

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -25,8 +25,7 @@ working_directory "/home/ia/workspace/pisource" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/home/ia/workspace/pisource/tmp/sockets/.unicorn.sock", :backlog => 64
-listen 3000, :tcp_nopush => true
+listen "/home/ia/workspace/pisource/tmp/sockets/unicorn.pisource.sock", :backlog => 64
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
