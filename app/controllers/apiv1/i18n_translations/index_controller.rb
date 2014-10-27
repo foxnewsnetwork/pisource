db41993cacee0f526ed84fce15917a6a7b5a6365
Thomas Chen <foxnewsnetwork@gmail.com>
2014-10-26 17:28:24 -0700

    accident space

--- app/controllers/apiv1/i18n_translations/index_controller.rb
+++ app/controllers/apiv1/i18n_translations/index_controller.rb
@@ -1,6 +1,6 @@
 class Apiv1::I18nTranslations::IndexController < ApplicationController
   def index
-    _force_all_lazy_translations_to_load
+    _force_all_lazy_translations_to_load  
     render json: { translations: I18n.backend.send(:translations), locale: I18n.locale }
   end
   private
